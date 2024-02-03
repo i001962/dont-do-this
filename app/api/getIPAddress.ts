f43936d3d1203d197956887e6ba133284dc95681
i001962@gmail.com <i001962@gmail.com>
2024-02-02 17:19:41 -0800

    priv 2

--- app/api/getIPAddress.ts
+++
@@ -1,7 +0,0 @@
-// pages/api/getIpAddress.ts
-
-export default (req, res) => {
-    const ipAddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    res.status(200).json({ ipAddress });
-  };
-  
